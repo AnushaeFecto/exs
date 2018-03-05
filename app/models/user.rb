93c12a4c17505816c60c7651935bf061c9682bd1
Sergio Mamede <sergiomamede21@gmail.com>
2018-03-05 16:42:41 +0000

    generating controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :items
+  has_many :deals
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end

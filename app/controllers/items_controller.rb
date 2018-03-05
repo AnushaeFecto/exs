d6da95971a1b70932f04bf096987aa1aed3621a6
Velina Cai <velina_30@yahoo.com>
2018-03-05 17:14:11 +0000

    items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,45 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+     if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    #redirect_to user_path(current_user)
+  end
+
+  def destroy
+    @item.destroy
+    #redirect_to user_path(current_user)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :category, :size, :color, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end

40217f857b28c1b75167a1c8085b99bbdd350560
Anton Lin <antonslin@aol.com>
2018-03-06 14:46:49 +0000

    stripe set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306130126) do
+ActiveRecord::Schema.define(version: 20180306141714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180306130126) do
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
     t.string "price_currency", default: "GBP", null: false
+    t.jsonb "payment"
     t.index ["user_id"], name: "index_deals_on_user_id"
   end
 

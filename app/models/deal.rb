63e8c313fb66621ec1332916f9c53039e6816bfd
Sergio Mamede <sergiomamede21@gmail.com>
2018-03-07 16:43:57 +0000

    deal-with-destroy-solved

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -2,7 +2,7 @@ class Deal < ApplicationRecord
   belongs_to :requester, class_name: 'User', foreign_key: 'requester_id'
   belongs_to :answerer, class_name: 'User', foreign_key: 'answerer_id'
   monetize :price_cents
-  has_many :deal_items
+  has_many :deal_items, dependent: :destroy
   has_many :items, through: :deal_items
 
 

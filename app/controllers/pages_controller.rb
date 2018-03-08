418e2cc053dc7dd2e41630eb49456ffa60839cf4
Velina Cai <velina_30@yahoo.com>
2018-03-08 12:50:30 +0000

    updated cards homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @items = Item.first(6)
+    @items = Item.limit(6).order("RANDOM()")
   end
 end

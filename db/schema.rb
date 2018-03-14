2dcdda2debb0ad77eb714c5294157b7e4489957f
Velina Cai <velina_30@yahoo.com>
2018-03-14 10:43:55 +0000

    searchposition

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 20180313135010) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "photo"
+    t.integer "cached_votes_total", default: 0
+    t.integer "cached_votes_score", default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "cached_votes_down", default: 0
+    t.integer "cached_weighted_score", default: 0
+    t.integer "cached_weighted_total", default: 0
+    t.float "cached_weighted_average", default: 0.0
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -70,6 +77,20 @@ ActiveRecord::Schema.define(version: 20180313135010) do
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", id: :serial, force: :cascade do |t|
+    t.string "votable_type"
+    t.integer "votable_id"
+    t.string "voter_type"
+    t.integer "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
 
   add_foreign_key "deal_items", "deals"
